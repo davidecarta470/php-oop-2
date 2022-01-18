d6f0a21a6f27636d5763e03f0db34506f6154fbd
davidecarta470 <cartadavi@gmail.com>
2022-01-18 17:34:54 +0100

    third commit correction fulldiscount

--- UtentePremium.php
+++ UtentePremium.php
@@ -7,8 +7,8 @@ class UtentePremium extends Utente{
   public function getDiscound(){
     if($this->numberOfProductsPurchased > 1){
        $this->discount=10;
-       $this->fullDiscount += $this->extradDiscount+$this->discount;
-       return "l'utente $this->name $this->lastname ha ricevuto uno sconto del $this->discount% per l'aquisto di $this->numberOfProductsPurchased prodotti, piu' un ulteriore sconto del $this->extraDiscount % per essere un utente premium.Lo sconto totale è pari a $this->fullDiscount<br>";
+       $this->fullDiscount = $this->extraDiscount + $this->discount;
+       return "l'utente $this->name $this->lastname ha ricevuto uno sconto del $this->discount% per l'aquisto di $this->numberOfProductsPurchased prodotti, piu' un ulteriore sconto del $this->extraDiscount % per essere un utente premium.Lo sconto totale è pari a $this->fullDiscount %<br>";
     }
     return "non sono previsti sconti per l'utente $this->name $this->lastname<br>";
   }
